5611ed8cab5fca4cea3ed118de2b795dcaf6e374
Tannuattri01 <161629643+Tannuattri01@users.noreply.github.com>
2025-11-21 00:28:26 +0530

    Rename buy function to Buy and update image paths

--- src/buy.jsx
+++ src/buy.jsx
@@ -1,40 +1,38 @@
 
 import React from "react";
 
-function buy() {
+function Buy() {
     return (
         // buy section
-        <div 
-         id="categories" className="main-buy-section">
+        <div id="categories" className="main-buy-section">
             <div className="buy-section-div1">
-                <img className="buy-section-img1" src="./images/coffee.png" alt="image" />
-                <img className="buy-section-img2" src="./images/xx.png" alt="" />
-                <img className="buy-section-img3" src="./images/black1-xx.png" alt="" />
-                <img className="buy-section-img4" src="./images/grey-xx.png" alt="" />
-                <img className="buy-section-img5" src="./images/black2xx.png" alt="" />
+                <img className="buy-section-img1" src="/coffee.png" alt="image" />
+                <img className="buy-section-img2" src="/xx.png" alt="" />
+                <img className="buy-section-img3" src="/black1-xx.png" alt="" />
+                <img className="buy-section-img4" src="/grey-xx.png" alt="" />
+                <img className="buy-section-img5" src="/black2xx.png" alt="" />
                 <h1 className="buy-section-div1-text">5</h1>
                 <div className="off">
-                    <h2 className="off" class>BUY 5 GET
-                    20% OFF</h2>
+                    <h2 className="off">BUY 5 GET 20% OFF</h2>
                 </div>
             </div>
             <div className="buy-section-div2">
-                <img className="buy-section-image1" src="./images/roll.png" alt="" />
-                <img className="buy-section-image2" src="./images/eye.png" alt="" />
-                <img className="buy-section-image3" src="./images/dog.png" alt="" />
-                <img className="buy-section-image4" src="./images/girl.png" alt="" />
-                <img className="buy-section-image5" src="./images/bhoo.png" alt="" />
-                <img className="buy-section-image6" src="./images/coffee2.png" alt="" />
-                <img className="buy-section-image7" src="./images/funny.png" alt="" />
-                <img className="buy-section-image8" src="./images/blur.png" alt="" />
-                <img className="buy-section-image9" src="./images/feet.png" alt="" />
+                <img className="buy-section-image1" src="/roll.png" alt="" />
+                <img className="buy-section-image2" src="/eye.png" alt="" />
+                <img className="buy-section-image3" src="/dog.png" alt="" />
+                <img className="buy-section-image4" src="/girl.png" alt="" />
+                <img className="buy-section-image5" src="/bhoo.png" alt="" />
+                <img className="buy-section-image6" src="/coffee2.png" alt="" />
+                <img className="buy-section-image7" src="/funny.png" alt="" />
+                <img className="buy-section-image8" src="/blur.png" alt="" />
+                <img className="buy-section-image9" src="/feet.png" alt="" />
                 <h1 className="buy-section-div2-text">10</h1>
                 <div className="off">
-                    <h2 className="off" class>BUY 10 GET
-                    23% OFF</h2>
+                    <h2 className="off">BUY 10 GET 23% OFF</h2>
                 </div>
             </div>
         </div>
     );
 }
-export default buy;
+
+export default Buy;
