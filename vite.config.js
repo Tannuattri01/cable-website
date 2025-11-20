a5d2f404df37215a9efe4c295c0328419686cf28
Tannuattri01 <161629643+Tannuattri01@users.noreply.github.com>
2025-11-20 22:28:37 +0530

    Change base path for deployment configuration

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react';
 const isGitHubPages = process.env.DEPLOY_ENV === 'GH_PAGES';
 
 export default defineConfig({
-  base:  './',
+ 
   plugins: [react()],
 });
