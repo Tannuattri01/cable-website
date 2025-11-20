bfd2ee515dc788353d8ff666841001f63df3f781
Tannuattri01 <161629643+Tannuattri01@users.noreply.github.com>
2025-11-21 01:15:28 +0530

    Update buy.jsx

--- src/buy.jsx
+++ src/buy.jsx
@@ -1,43 +1,41 @@
 
+
 import React from "react";
 
-
-function Buy() {
-  return (
-    <div id="categories" className="main-buy-section">
-      <div className="buy-section-div1">
-        <div className="buy-images-wrapper">
-          <img className="buy-section-img1" src="/coffee.png" alt="image" />
-          <img className="buy-section-img2" src="/xx.png" alt="" />
-          <img className="buy-section-img3" src="/black1-xx.png" alt="" />
-          <img className="buy-section-img4" src="/grey-xx.png" alt="" />
-          <img className="buy-section-img5" src="/black2xx.png" alt="" />
+function buy() {
+    return (
+        // buy section
+        <div 
+         id="categories" className="main-buy-section">
+            <div className="buy-section-div1">
+                <img className="buy-section-img1" src="./images/coffee.png" alt="image" />
+                <img className="buy-section-img2" src="./images/xx.png" alt="" />
+                <img className="buy-section-img3" src="./images/black1-xx.png" alt="" />
+                <img className="buy-section-img4" src="./images/grey-xx.png" alt="" />
+                <img className="buy-section-img5" src="./images/black2xx.png" alt="" />
+                <h1 className="buy-section-div1-text">5</h1>
+                <div className="off">
+                    <h2 className="off" class>BUY 5 GET
+                    20% OFF</h2>
+                </div>
+            </div>
+            <div className="buy-section-div2">
+                <img className="buy-section-image1" src="./images/roll.png" alt="" />
+                <img className="buy-section-image2" src="./images/eye.png" alt="" />
+                <img className="buy-section-image3" src="./images/dog.png" alt="" />
+                <img className="buy-section-image4" src="./images/girl.png" alt="" />
+                <img className="buy-section-image5" src="./images/bhoo.png" alt="" />
+                <img className="buy-section-image6" src="./images/coffee2.png" alt="" />
+                <img className="buy-section-image7" src="./images/funny.png" alt="" />
+                <img className="buy-section-image8" src="./images/blur.png" alt="" />
+                <img className="buy-section-image9" src="./images/feet.png" alt="" />
+                <h1 className="buy-section-div2-text">10</h1>
+                <div className="off">
+                    <h2 className="off" class>BUY 10 GET
+                    23% OFF</h2>
+                </div>
+            </div>
         </div>
-        <h1 className="buy-section-div1-text">5</h1>
-        <div className="off">
-          <h2 className="off-text">BUY 5 GET 20% OFF</h2>
-        </div>
-      </div>
-
-      <div className="buy-section-div2">
-        <div className="buy-images-wrapper">
-          <img className="buy-section-image1" src="/roll.png" alt="" />
-          <img className="buy-section-image2" src="/eye.png" alt="" />
-          <img className="buy-section-image3" src="/dog.png" alt="" />
-          <img className="buy-section-image4" src="/girl.png" alt="" />
-          <img className="buy-section-image5" src="/bhoo.png" alt="" />
-          <img className="buy-section-image6" src="/coffee2.png" alt="" />
-          <img className="buy-section-image7" src="/funny.png" alt="" />
-          <img className="buy-section-image8" src="/blur.png" alt="" />
-          <img className="buy-section-image9" src="/feet.png" alt="" />
-        </div>
-        <h1 className="buy-section-div2-text">10</h1>
-        <div className="off">
-          <h2 className="off-text">BUY 10 GET 23% OFF</h2>
-        </div>
-      </div>
-    </div>
-  );
+    );
 }
-
-export default Buy;
+export default buy;
